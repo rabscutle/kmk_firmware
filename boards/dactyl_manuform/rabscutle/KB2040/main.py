87e529f7fd7d5360b2ecb609b9f781edfa5af0ac
rabscutle <pkhtexas@yahoo.com>
2023-09-20 00:17:34 -0500

    Update main.py

--- boards/dactyl_manuform/rabscutle/KB2040/main.py
+++ boards/dactyl_manuform/rabscutle/KB2040/main.py
@@ -1,32 +1,30 @@
 from kb import MyKeyboard
+import board
 
-from kmk.keys import KC
-
-from kmk.modules.layers import Layers
-from kmk.modules.split import Split, SplitSide
-from kmk.modules.holdtap import HoldTap
 from kmk.extensions.media_keys import MediaKeys
+from kmk.keys import KC
+from kmk.modules.holdtap import HoldTap
+from kmk.modules.layers import Layers
 from kmk.handlers.sequences import send_string
+from kmk.modules.split import Split, SplitSide, SplitType
 
 keyboard = MyKeyboard()
-
 
 keyboard.modules.append(Layers())
 keyboard.modules.append(HoldTap())
 keyboard.extensions.append(MediaKeys())
 
 
-#split = Split(split_side=SplitSide.LEFT)
 split = Split(
-    split_flip=False,  # If both halves are the same, but flipped, set this True
-    split_side=None,  # Sets if this is to SplitSide.LEFT or SplitSide.RIGHT, or use EE hands
     #split_type=SplitType.UART,  # Defaults to UART
-    split_target_left=True,  # Assumes that left will be the one on USB. Set to False if it will be the right
+    split_side=SplitSide.LEFT,  # Sets if this is to SplitSide.LEFT or SplitSide.RIGHT, or use EE hands    
+    split_flip=True,  # If both halves are the same, but flipped, set this True
+    #split_target_left=True,  # Assumes that left will be the one on USB. Set to False if it will be the right
     #uart_interval=20,  # Sets the uarts delay. Lower numbers draw more power
-    data_pin=None,  # The primary data pin to talk to the secondary device with
-    data_pin2=None,  # Second uart pin to allow 2 way communication
-    #uart_flip=True,  # Reverses the RX and TX pins if both are provided
+    data_pin=board.TX,  # The primary data pin to talk to the secondary device with
+    data_pin2=board.RX,  # Second uart pin to allow 2 way communication
     use_pio=True,  # Use RP2040 PIO implementation of UART. Required if you want to use other pins than RX/TX
+    uart_flip=True,  # Reverses the RX and TX pins if both are provided
 )
 
 keyboard.modules.append(split)
