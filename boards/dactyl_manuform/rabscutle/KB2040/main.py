f5320a18eb5da5052f455379474f4bd4497a28eb
rabscutle <pkhtexas@yahoo.com>
2023-09-20 09:25:23 -0500

    Update main.py

--- boards/dactyl_manuform/rabscutle/KB2040/main.py
+++ boards/dactyl_manuform/rabscutle/KB2040/main.py
@@ -16,15 +16,14 @@ keyboard.extensions.append(MediaKeys())
 
 
 split = Split(
-    #split_type=SplitType.UART,  # Defaults to UART
-    split_side=SplitSide.LEFT,  # Sets if this is to SplitSide.LEFT or SplitSide.RIGHT, or use EE hands    
+    split_type=SplitType.UART,  # Defaults to UART
+    split_side=None, # Sets if this is to SplitSide.LEFT or SplitSide.RIGHT, or use EE hands
     split_flip=True,  # If both halves are the same, but flipped, set this True
-    #split_target_left=True,  # Assumes that left will be the one on USB. Set to False if it will be the right
-    #uart_interval=20,  # Sets the uarts delay. Lower numbers draw more power
+    uart_interval=20,  # Sets the uarts delay. Lower numbers draw more power
     data_pin=board.TX,  # The primary data pin to talk to the secondary device with
     data_pin2=board.RX,  # Second uart pin to allow 2 way communication
     use_pio=True,  # Use RP2040 PIO implementation of UART. Required if you want to use other pins than RX/TX
-    uart_flip=True,  # Reverses the RX and TX pins if both are provided
+    uart_flip=False,  # Reverses the RX and TX pins if both are provided
 )
 
 keyboard.modules.append(split)
